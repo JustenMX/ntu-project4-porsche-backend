92c8b4c9f220f786c96b89166856f31879eff7e9
Justen Manni <me@justenmanni.dev>
2023-11-14 00:49:42 +0800

    Project Initialization

---
+++ src/test/java/com/porsche/porschebackend/PorscheBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porsche.porschebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PorscheBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
